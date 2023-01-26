f61267ad40e76558e92852a1ff2e9e079e6ca1ef
David McCloskey <dave@juicelabs.co>
2023-01-26 13:12:04 -0600

    Build with multiple CUDA versions for server. CUDA must be <= host CUDA version.

--- docker/server/build.bat
+++ docker/server/build.bat
@@ -1,1 +1,2 @@
-docker build --pull --rm --build-arg BASE_CONTAINER=nvidia/cuda:11.8.0-cudnn8-runtime-ubuntu20.04 --build-arg JUICE_VERSION=%1 -f "./Dockerfile" -t juicelabs/server:%1 "."
+docker build --pull --rm --build-arg BASE_CONTAINER=nvidia/cuda:11.7.0-cudnn8-runtime-ubuntu20.04 --build-arg JUICE_VERSION=%1 -f "./Dockerfile" -t juicelabs/server:11.7-%1 "."
+docker build --pull --rm --build-arg BASE_CONTAINER=nvidia/cuda:11.8.0-cudnn8-runtime-ubuntu20.04 --build-arg JUICE_VERSION=%1 -f "./Dockerfile" -t juicelabs/server:11.8-%1 "."
