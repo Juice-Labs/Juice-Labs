25a64c198cf1f22d7e1ea5c5dec81191860886ca
Ramon Steenson <114740458+ramon-juicelabs@users.noreply.github.com>
2023-08-17 17:27:08 +1200

    Ignore legit shutdown errors (via ctrl-c etc)

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -124,10 +124,16 @@ func (server *Server) Run(group task.Group) error {
 	}
 
 	group.GoFn("HTTP Listen", func(group task.Group) error {
+		var err error
 		if server.tlsConfig != nil {
-			return httpServer.ListenAndServeTLS("", "")
+			err = httpServer.ListenAndServeTLS("", "")
 		} else {
-			return httpServer.ListenAndServe()
+			err = httpServer.ListenAndServe()
+		}
+		if err == http.ErrServerClosed {
+			return nil
+		} else {
+			return err
 		}
 	})
 
