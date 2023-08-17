bc36f916f45517c270af6978e8b6ccc21615946a
Ramon Steenson <114740458+ramon-juicelabs@users.noreply.github.com>
2023-08-17 21:54:23 +1200

    Log route access to info level rather than debug

--- pkg/logger/middleware.go
+++ pkg/logger/middleware.go
@@ -9,7 +9,7 @@ import (
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		Debugf("%s %s %s", r.RemoteAddr, r.Method, r.RequestURI)
+		Infof("%s %s %s", r.RemoteAddr, r.Method, r.RequestURI)
 
 		next.ServeHTTP(w, r)
 	})
